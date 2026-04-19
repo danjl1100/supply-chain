071feb7e19039a6367d4f5c3f55f2aa531a2aa00
Daniel Lambert <danjl1100@gmail.com>
2026-04-18 23:44:03 -0500

    bail on cargo-vet error

--- update.sh
+++ update.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
+# bail on `cargo-vet` error
+set -e
+
 cargo vet aggregate sources.list > audits.toml
 
+# continue, capturing exit code
+set +e
+
 git diff --exit-code audits.toml >/dev/null; rv=$?
+
 if [ $rv -eq 0 ]; then
   echo "Up to date"
 else
