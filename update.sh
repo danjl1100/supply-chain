5fe5341078d15cf0148854b309a092ed9119143a
Daniel Lambert <danjl1100@gmail.com>
2026-04-18 22:32:57 -0500

    add next steps to update script

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 cargo vet aggregate sources.list > audits.toml
 
-git status
+git diff --exit-code audits.toml >/dev/null; rv=$?
+if [ $rv -eq 0 ]; then
+  echo "Up to date"
+else
+  echo "Recommended actions:"
+  echo "    git add -p"
+  echo "    git commit -m \"update from sources\""
+fi
